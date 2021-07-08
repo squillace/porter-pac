ff72602b56440b8e7408d52bce6e4d1977f7beaa
ralph squillace <ralph@squillace.com>
2021-07-08 18:02:00 -0400

    updating the path to pac

--- pkg/pac/build.go
+++ pkg/pac/build.go
@@ -24,9 +24,11 @@ type MixinConfig struct {
 // This is an example. Replace the following with whatever steps are needed to
 // install required components into
 const dockerfileLines = `RUN apt-get update && \
- apt-get install zip unzip gpg -y && \
-cnab/app/dotnet-install.sh --version 3.1.409
-chmod +x tools/pac`
+apt-get install zip unzip gpg tree curl -y && \
+mkdir -p cnab/app/ && \
+curl -l https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh -o cnab/app/dotnet-install.sh && \ 
+chmod +x cnab/app/dotnet-install.sh && \
+./cnab/app/dotnet-install.sh --version 5.0.301`
 
 // Build will generate the necessary Dockerfile lines
 // for an invocation image using this mixin
@@ -50,7 +52,9 @@ func (m *Mixin) Build() error {
 	}
 
 	fmt.Fprintf(m.Out, dockerfileLines)
-
+	fmt.Fprintf(m.Out, "\nENV DOTNET_ROOT=/root/.dotnet\n")
+	fmt.Fprintf(m.Out, "\nENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1")
+	fmt.Fprintf(m.Out, "\nENV PATH=$HOME/cnab/app/tools/:${PATH}")
 	// Example of pulling and defining a client version for your mixin
 	// fmt.Fprintf(m.Out, "\nRUN curl https://get.helm.sh/helm-%s-linux-amd64.tar.gz --output helm3.tar.gz", m.ClientVersion)
 
