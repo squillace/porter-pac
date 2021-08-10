f06277c20ca310daf763975643c3eacd8df43b10
ralph squillace <ralph@squillace.com>
2021-08-10 12:50:10 -0400

    updated to handle client cli version; 0.1.3 bump

--- pkg/pac/build.go
+++ pkg/pac/build.go
@@ -29,9 +29,7 @@ mkdir -p cnab/app/ && \
 curl -l https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh -o cnab/app/dotnet-install.sh && \ 
 chmod +x cnab/app/dotnet-install.sh && \
 ./cnab/app/dotnet-install.sh --version 5.0.301 && \
-mkdir -p /cnab/app/tools && \
-curl -L https://www.nuget.org/api/v2/package/Microsoft.PowerApps.CLI.Core.linux-x64/1.8.5 --output /cnab/app/pac.nuget && \
-cd /cnab/app/ && unzip /cnab/app/pac.nuget && chmod +x /cnab/app/tools/pac`
+mkdir -p /cnab/app/tools`
 
 // Build will generate the necessary Dockerfile lines
 // for an invocation image using this mixin
@@ -54,12 +52,13 @@ func (m *Mixin) Build() error {
 		m.ClientVersion = suppliedClientVersion
 	}
 
-	fmt.Fprintf(m.Out, dockerfileLines)
-	fmt.Fprintf(m.Out, "\nENV DOTNET_ROOT=/root/.dotnet\n")
-	fmt.Fprintf(m.Out, "\nENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1")
-	fmt.Fprintf(m.Out, "\nENV PATH=$HOME/cnab/app/tools/:${PATH}")
+	fmt.Fprintln(m.Out, dockerfileLines)
+	fmt.Fprintln(m.Out, "ENV DOTNET_ROOT=/root/.dotnet")
+	fmt.Fprintln(m.Out, "ENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1")
+	fmt.Fprintln(m.Out, "ENV PATH=$HOME/cnab/app/tools/:${PATH}")
 	// Example of pulling and defining a client version for your mixin
-	// fmt.Fprintf(m.Out, "\nRUN curl https://get.helm.sh/helm-%s-linux-amd64.tar.gz --output helm3.tar.gz", m.ClientVersion)
+	fmt.Fprintf(m.Out, "RUN curl -L https://www.nuget.org/api/v2/package/Microsoft.PowerApps.CLI.Core.linux-x64/%s --output /cnab/app/pac.nuget", m.ClientVersion)
+	fmt.Fprintln(m.Out, "\nRUN cd /cnab/app/ && unzip /cnab/app/pac.nuget && chmod +x /cnab/app/tools/pac")
 
 	return nil
 }
